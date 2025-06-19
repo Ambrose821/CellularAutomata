1505c35497255f0d9dbc65e40c3702be4447ba73
Ambrose821 <mclaughlinambrose@gmail.com>
2025-06-18 22:08:19 -0700

    Create Initial Simulation loop

--- main.cpp
+++ main.cpp
@@ -10,19 +10,21 @@ int main() {
     cout<<"Loading Game of Life";
     const int screen_width=800;
     const int screen_height=800;
+
+    int FPS = 12;
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(screen_width,screen_height,"Ambrose's Game of Life");
-    SetTargetFPS(60);
+
+    SetTargetFPS(FPS); //Lower Fps to properly visualize the simulation
     while(WindowShouldClose()==false){
         BeginDrawing();
-        ClearBackground(BLACK);
-
-        //1 Event Handling
-
-
-        //2 Perform Updates
+        ClearBackground({255,0,0,255});
+        //1. Event Handling
 
 
+        //2. Update State
+
+        //3. Draw Graphics 
         //Draw Graphics
         EndDrawing();
         
