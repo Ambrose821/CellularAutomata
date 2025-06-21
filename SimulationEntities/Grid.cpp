b04a31ae392ff7af4226eb387fa0cee1a577203a
Ambrose821 <mclaughlinambrose@gmail.com>
2025-06-21 13:59:16 -0700

    Added life check for neighbor cells

--- SimulationEntities/Grid.cpp
+++ SimulationEntities/Grid.cpp
@@ -6,10 +6,66 @@ void  Grid::Draw()
     for(int i =0; i <this->rows; i++){
         for(int j =0; j<this->columns; j++){
              if(this->cells[i][j] == 1){
-               Color color = cells[i][j] ? Color{0,255,0,255} : Color{55,55,55,255};
+               Color color = cells[i][j] ? Color{0,255,0,255} : BLACK;
              }
-             Color color = j%2 ==0 && i%2 ==0 ? Color{0,255,0,255} : Color{55,55,55,255};
+             Color color = j%2 ==0 && i%2 ==0 ? Color{0,255,0,255} : BLACK;
                 DrawRectangle(j*this->cellSize,i*this->cellSize,this->cellSize,this->cellSize,color);
         }
     }
+}
+
+bool Grid::isSafePosition(int row,int col)
+{
+  return(row >= 0 && row< this->rows && col >=0 and col< this->columns);
+}
+int Grid::checkLife(int row, int col){
+    int liveNeighbors =0;
+    
+    //Above Neighbors
+    if(isSafePosition(row-1,col-1)){
+      if(cells[row-1][col-1] == 1){
+      liveNeighbors +=1;
+      }
+    }
+    if(isSafePosition(row-1,col)){
+      if(cells[row-1][col] == 1){
+      liveNeighbors +=1;
+      }
+    }
+    if(isSafePosition(row-1,col+1)){
+      if(cells[row-1][col+1] == 1){
+      liveNeighbors +=1;
+      } 
+    }
+    //Below Neighbors
+    if(isSafePosition(row+1,col-1)){
+      if(cells[row+1][col-1] == 1){
+      liveNeighbors +=1;
+      } 
+    }
+      if(isSafePosition(row+1,col)){
+      if(cells[row+1][col] == 1){
+      liveNeighbors +=1;
+      } 
+    }
+      if(isSafePosition(row+1,col+1)){
+      if(cells[row+1][col+1] == 1){
+      liveNeighbors +=1;
+      } 
+    }
+
+    //Horizontal Neighbors
+
+      if(isSafePosition(row,col-1)){
+      if(cells[row][col-1] == 1){
+      liveNeighbors +=1;
+      } 
+    }
+
+      if(isSafePosition(row,col+1)){
+      if(cells[row][col+1] == 1){
+      liveNeighbors +=1;
+      } 
+    }
+    return liveNeighbors;
 }
--- SimulationEntities/Grid.hpp
+++ SimulationEntities/Grid.hpp
@@ -10,12 +10,19 @@ class Grid{
         Grid(int width, int height, int cellSize): rows(height/cellSize),columns(width/cellSize),cellSize(cellSize), cells(rows,vector<int>(columns,0)){};
 
         void Draw();
+        int checkLife(int row,int col);
+
+       
+
+
 
         private:
-        int rows;
-        int columns;
-        int cellSize;
-        vector<vector<int>> cells; 
+            int rows;
+            int columns;
+            int cellSize;
+            vector<vector<int>> cells; 
+             //Check if a neighbor is within the bounds of grid
+            bool isSafePosition(int row, int col);
 
 };
  
